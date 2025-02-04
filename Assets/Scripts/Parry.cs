ec112aad37f494043de4ebbdabb58cfcee3fe4d7
Alperen Erol <alpereneroltr@gmail.com>
2025-02-04 22:40:05 +0300

    sword && parry collider fix

--- Assets/Scripts/Parry.cs
+++ Assets/Scripts/Parry.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.InputSystem;
 
 public class Parry : MonoBehaviour
 {
     public AudioSource audioSource;
     public AudioClip[] parrySounds;
+    private Collider collider;
 
     public WeaponAnim wa;
+
+    private void Start()
+    {
+        collider = GetComponent<Collider>();
+        collider.enabled = false;
+    }
+
+    private void Update()
+    {
+        CheckBlock();
+    }
 
     private void OnCollisionEnter(Collision other)
     {
@@ -21,5 +34,13 @@ public class Parry : MonoBehaviour
     public void HandleParry()
     {
         audioSource.PlayOneShot(parrySounds[Random.Range(0, parrySounds.Length)]);
+    }
+
+    public void CheckBlock()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+            collider.enabled = true;
+        else if (Input.GetKeyUp(KeyCode.Mouse1))
+            collider.enabled = false;
     }
 }
--- Assets/Scripts/WeaponAnim.cs
+++ Assets/Scripts/WeaponAnim.cs
@@ -44,7 +44,6 @@ public class WeaponAnim : MonoBehaviour
             ResetComboStep();
             canReset = false;
         }
-        Debug.Log(canAttack + " " + isAttacking);
     }
 
     private void OnCollisionEnter(Collision other)
