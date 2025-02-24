f780d91963d275407e1803c74b1fad7d1488861d
Alperen Erol <alpereneroltr@gmail.com>
2025-02-24 21:25:20 +0300

    .

--- Assets/Scripts/StateMachine/PatrolAIBehaviour.cs
+++ Assets/Scripts/StateMachine/PatrolAIBehaviour.cs
@@ -2,15 +2,66 @@ using UnityEngine;
 
 public class PatrolAIBehaviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private AIState currentState;
+    private AIStateType currentStateType;
+
+    private Patrol patrolState;
+    private Chase chaseState;
+
+    private void Start()
     {
-        
+        // Initialize states
+        patrolState = new Patrol();
+        chaseState = new Chase();
+
+        // Set initial state
+        ChangeState(AIStateType.Patrol);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Update the current state
+        if (currentState != null)
+        {
+            currentState.Update();
+        }
+
+        // Example: Switch state based on some condition
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (currentStateType == AIStateType.Patrol)
+            {
+                ChangeState(AIStateType.Chase);
+            }
+            else
+            {
+                ChangeState(AIStateType.Patrol);
+            }
+        }
+    }
+
+    public void ChangeState(AIStateType newState)
+    {
+        // Exit the current state
+        if (currentState != null)
+        {
+            currentState.Exit();
+        }
+
+        // Set the new state
+        switch (newState)
+        {
+            case AIStateType.Patrol:
+                currentState = patrolState;
+                break;
+            case AIStateType.Chase:
+                currentState = chaseState;
+                break;
+        }
+
+        currentStateType = newState;
+
+        // Enter the new state
+        currentState.Enter();
     }
 }
--- Assets/Scripts/StateMachine/PatrolState.cs
+++ Assets/Scripts/StateMachine/PatrolState.cs
@@ -4,19 +4,16 @@ public class Patrol : AIState
 {
     public void Enter()
     {
-        Debug.Log("Entering Patrol State");
-        // Initialize patrol behavior (e.g., set waypoints, start moving)
+
     }
 
     public void Update()
     {
-        Debug.Log("Patrolling...");
-        // Implement patrol logic (e.g., move between waypoints)
+
     }
 
     public void Exit()
     {
-        Debug.Log("Exiting Patrol State");
-        // Clean up patrol behavior (e.g., stop moving)
+
     }
 }
